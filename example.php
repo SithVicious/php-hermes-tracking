e8a59bd155edb809a2be42f8f9f406d13e1ee4f8
SithVicious <paul@nwit.info>
2021-05-30 02:34:22 +1000

    Update example.php

--- example.php
+++ example.php
@@ -5,5 +5,8 @@ require_once('classes/class.hermes.php');
 
 $tracking = new Tracking_Hermes();
 
-print_r($tracking->getTracking("0840683668518683")); # Will return redacted results
-print_r($tracking->getTracking("0840683668518683", "S9 1XX")); # Providing postcode will give full results, address details, GPS coords of delivery and photos if available.
+# Will return basic, redacted results
+print_r($tracking->getTracking("1234567890123456")); 
+
+# Providing postcode will give full results, address details, GPS coords of delivery and photos if available.
+print_r($tracking->getTracking("1234567890123456", "S9 1XX")); 
