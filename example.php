70bbdbc71bd77efed066b4aae7bdd82fad977d64
SithVicious <paul@nwit.info>
2021-05-30 02:27:34 +1000

    Create example.php

---
+++ example.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: application/json');
+require_once('classes/class.hermes.php');
+
+
+$tracking = new Tracking_Hermes();
+
+print_r($tracking->getTracking("0840683668518683")); # Will return redacted results
+print_r($tracking->getTracking("0840683668518683", "S9 1XX")); # Providing postcode will give full results, address details, GPS coords of delivery and photos if available.
