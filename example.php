f3fb5adc584688f10a8d14def570b593e101b2a8
SithVicious <paul@nwit.info>
2021-05-30 12:45:47 +1000

    Remove json content type - Not needed and was initially used in testing

--- example.php
+++ example.php
@@ -1,9 +1,9 @@
 <?php
-header('Content-Type: application/json');
 require_once('classes/class.hermes.php');
 
 
-$tracking = new Tracking_Hermes();
+$tracking = new Tracking_Hermes(); # Attempt to use hard coded apiKey taken from Hermes official website 
+#$tracking = new Tracking_Hermes("YOUR_API_KEY"); # Use your own apiKey if you have one
 
 # Will return basic, redacted results
 print_r($tracking->getTracking("1234567890123456")); 
